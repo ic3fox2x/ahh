18b247c58f8eaf117331a200f476e3cbb3da1bbb
Tanish <ahhtanish@gmail.com>
2022-03-24 17:46:27 +0530

    ✨

---
+++ grade calculator/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks;
+    printf("Enter your marks?\n");
+    scanf("%d", &marks);
+    if (marks < 0 || marks > 100)
+    {
+        printf("Invalid Entry!");
+    }
+    else if (marks >= 90)
+    {
+        printf("Grade A");
+    }
+    else if (marks >= 80 && marks < 90)
+    {
+        printf("Grade B");
+    }
+    else if (marks >= 70 && marks < 80)
+    {
+        printf("Grade C");
+    }
+    else if (marks >= 60 && marks < 70)
+    {
+        printf("Grade D");
+    }
+    return 0;
+}
