ed654f4388ac61dc03927e43a4ce7b620a79ecec
Tanish <ahhtanish@gmail.com>
2022-03-24 16:56:58 +0530

    ✨

---
+++ odd and even/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Give me a number? \n");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+        printf("%d is even \n", a);
+    }
+    else
+    {
+        printf("%d is odd \n", a);
+    }
+    return 0;
+}
